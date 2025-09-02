b1e1c2eda234ea0f1e847b4ac8ebd63e038ead1a
Lewis Smith <66628534+PKThunda291@users.noreply.github.com>
2025-09-02 10:20:16 +0100

    Added input validation for the url

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-const inputField = document.getElementById("image-input")
 
+const inputField = document.getElementById("image-input")
+const validExtentions = [".png", ".jpeg", ".jpg", ".gif"];
 
 function addImage(){
     event.preventDefault();
-    console.log(inputField.value);
 
-    inputField.value = "";
+    // Input Validation
+    // If empty input
+    if (inputField.value == "") return;
+    // If url doesn't exist
+    if (!checkURLExists(inputField.value)) return;
+    // if URL isn't image
+    if (!checkExtention(inputField.value)) return;
+
+    
+}
+
+// Checks that the file is an image
+function checkExtention(src){
+    let isValid = false;
+    validExtentions.forEach(element => {
+        if (src.endsWith(element)) isValid = true;
+    });
+    return isValid;
+}
+
+// Checks that the file exists in the directory
+function checkURLExists(src){
+    let request = new XMLHttpRequest();
+
+    // Initialise and send HTTPS request
+    request.open('HEAD', src, false);
+    request.send();
+    // If request cant find resource, return false, otherwise return true
+    return request.status != 404;
 }
